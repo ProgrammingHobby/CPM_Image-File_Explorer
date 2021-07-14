201c793e21920af4798decb38b4311c0c0a326f2
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2021-07-14 19:03:05 +0200

    Initialer Projekt Commit

---
+++ CIFE/MainWindow.cpp
@@ -0,0 +1,31 @@
+/***********************************************************************************
+**
+**	Copyright (C) 14.07.2021 Uwe Merker Germany
+**
+**	Diese Datei ist Teil von CPM_Image_File_Explorer.
+**
+**	CPM_Image_File_Explorer ist Freie Software: Sie können es unter den Bedingungen
+**	der GNU General Public License, wie von der Free Software Foundation,
+**	Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+**	veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+**
+**	CPM_Image_File_Explorer wird in der Hoffnung, dass es nützlich sein wird, aber
+**	OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+**	Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+**	Siehe die GNU General Public License für weitere Details.
+**
+**	Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+**	Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+**
+***********************************************************************************/
+
+#include "MainWindow.h"
+// --------------------------------------------------------------------------------
+MainWindow::MainWindow(wxWindow *parent) : Ui_MainWindow(parent) {
+}
+
+// --------------------------------------------------------------------------------
+MainWindow::~MainWindow() {
+}
+
+// --------------------------------------------------------------------------------
---
+++ CIFE/MainWindow.h
@@ -0,0 +1,34 @@
+/***********************************************************************************
+**
+**	Copyright (C) 14.07.2021 Uwe Merker Germany
+**
+**	Diese Datei ist Teil von CPM_Image_File_Explorer.
+**
+**	CPM_Image_File_Explorer ist Freie Software: Sie können es unter den Bedingungen
+**	der GNU General Public License, wie von der Free Software Foundation,
+**	Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+**	veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+**
+**	CPM_Image_File_Explorer wird in der Hoffnung, dass es nützlich sein wird, aber
+**	OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+**	Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+**	Siehe die GNU General Public License für weitere Details.
+**
+**	Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+**	Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+**
+***********************************************************************************/
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+// --------------------------------------------------------------------------------
+#include "Ui_MainWindow.h"
+// --------------------------------------------------------------------------------
+class MainWindow : public Ui_MainWindow {
+    public:
+        MainWindow(wxWindow *parent);
+        virtual ~MainWindow();
+};
+
+// --------------------------------------------------------------------------------
+#endif // MAINWINDOW_H
