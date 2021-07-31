a61d2bffabde505891edf59364b3ddc403744fb6
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2021-07-31 17:48:29 +0200

    Klasse 'ImageFileTools' erstellt.

---
+++ CIFE/ImageFileTools.cpp
@@ -0,0 +1,30 @@
+/***********************************************************************************
+**
+**	Copyright (C) 31.07.2021 Uwe Merker Germany
+**
+**	Diese Datei ist Teil von CPM_Image_File_Explorer.
+**
+**	CPM_Image_File_Explorer ist Freie Software: Sie können es unter den Bedingungen
+**	der GNU General Public License, wie von der Free Software Foundation,
+**	Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+**	veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+**
+**	CPM_Image_File_Explorer wird in der Hoffnung, dass es nützlich sein wird, aber
+**	OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+**	Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+**	Siehe die GNU General Public License für weitere Details.
+**
+**	Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+**	Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+**
+***********************************************************************************/
+#include "ImageFileTools.h"
+// --------------------------------------------------------------------------------
+ImageFileTools::ImageFileTools() {
+}
+
+// --------------------------------------------------------------------------------
+ImageFileTools::~ImageFileTools() {
+}
+
+// --------------------------------------------------------------------------------
