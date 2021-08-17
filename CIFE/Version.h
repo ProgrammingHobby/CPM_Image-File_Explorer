7f2f6f64538ebf7faf40dc9d02e26a846ff25224
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2021-08-17 21:36:14 +0200

    Datei Version.h mit Versionsinformationen erzeugt.

---
+++ CIFE/Version.h
@@ -0,0 +1,27 @@
+/***********************************************************************************
+**
+**	Copyright (C) 04.08.2021 Uwe Merker Germany
+**
+**	Diese Datei ist Teil von CPM_Image-File_Explorer.
+**
+**	CPM_Image-File_Explorer ist Freie Software: Sie können es unter den Bedingungen
+**	der GNU General Public License, wie von der Free Software Foundation,
+**	Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+**	veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+**
+**	CPM_Image-File_Explorer wird in der Hoffnung, dass es nützlich sein wird, aber
+**	OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+**	Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+**	Siehe die GNU General Public License für weitere Details.
+**
+**	Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+**	Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+**
+***********************************************************************************/
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION_STRING "0.0.1"
+
+#endif
+
