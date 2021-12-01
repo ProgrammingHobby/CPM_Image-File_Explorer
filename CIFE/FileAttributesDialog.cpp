dadfef406371e0f8dbec334702bb552aba62d445
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2021-12-01 18:32:52 +0100

    Dialog 'File Attributes' erstellt.

---
+++ CIFE/FileAttributesDialog.cpp
@@ -0,0 +1,32 @@
+/***********************************************************************************
+**
+**	Copyright (C) 01.12.2021 Uwe Merker Germany
+**
+**	Diese Datei ist Teil von CPM_Image-File_Explorer.
+**
+**	CPM_Image-File_Explorer ist Freie Software: Sie können es unter den Bedingungen
+**	der GNU General Public License, wie von der Free Software Foundation,
+**	Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+**	veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+**
+**	CPM_Image-File_Explorer wird in der Hoffnung, dass es nützlich sein wird, aber
+**	OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+**	Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+**	Siehe die GNU General Public License für weitere Details.
+**
+**	Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+**	Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+**
+***********************************************************************************/
+
+#include "FileAttributesDialog.h"
+// --------------------------------------------------------------------------------
+FileAttributesDialog::FileAttributesDialog(wxWindow *parent)
+    : Ui_FileAttributesDialog(parent) {
+}
+
+// --------------------------------------------------------------------------------
+FileAttributesDialog::~FileAttributesDialog() {
+}
+
+// --------------------------------------------------------------------------------
