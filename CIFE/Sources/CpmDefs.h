d3e44f9bbe99bfa33a20d32723ffb19f79f54911
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2022-10-19 18:55:49 +0200

    File-Header License Informationen angepasst.

--- CIFE/Sources/CpmDefs.h
+++ CIFE/Sources/CpmDefs.h
@@ -1,23 +1,19 @@
-/***********************************************************************************
-**
-**	Copyright (C) 29.12.2021 Uwe Merker Germany
-**
-**	Diese Datei ist Teil von CPM_Image-File_Explorer.
-**
-**	CPM_Image-File_Explorer ist Freie Software: Sie können es unter den Bedingungen
-**	der GNU General Public License, wie von der Free Software Foundation,
-**	Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
-**	veröffentlichten Version, weiterverbreiten und/oder modifizieren.
-**
-**	CPM_Image-File_Explorer wird in der Hoffnung, dass es nützlich sein wird, aber
-**	OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
-**	Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
-**	Siehe die GNU General Public License für weitere Details.
-**
-**	Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
-**	Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
-**
-***********************************************************************************/
+/*
+ * CpmDefs.h is part of CIFE
+ * Copyright (c) 2021  Uwe Merker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef CPMDEFS_H
 #define CPMDEFS_H
