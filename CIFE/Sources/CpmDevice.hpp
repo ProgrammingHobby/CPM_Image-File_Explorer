fdaa9c1a3a97c346a74be22d3406fa68b44c5c64
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2022-10-19 19:28:59 +0200

    Grundgerüst für Klasse 'CpmDevice' erstellt.

---
+++ CIFE/Sources/CpmDevice.hpp
@@ -0,0 +1,25 @@
+/*
+ * CpmDevice.hpp is part of CIFE
+ * Copyright (c) 2022  Uwe Merker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CPMDEVICE_HPP_
+#define CPMDEVICE_HPP_
+
+class CpmDevice
+    {
+    };
+
+#endif /* CPMDEVICE_HPP_ */
