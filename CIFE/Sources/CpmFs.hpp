6fefc0accef25c069bf0781ea329062561419ff6
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2022-10-20 18:45:24 +0200

    Grundgerüst für Klasse 'CpmFs' erstellt.

---
+++ CIFE/Sources/CpmFs.hpp
@@ -0,0 +1,25 @@
+/*
+ * CpmFs.hpp is part of CIFE
+ * Copyright (c) 2022  Uwe Merker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CPMFS_HPP_
+#define CPMFS_HPP_
+
+class CpmFs
+    {
+    };
+
+#endif /* CPMFS_HPP_ */
