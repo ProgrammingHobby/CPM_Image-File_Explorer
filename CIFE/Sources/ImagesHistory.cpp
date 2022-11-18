c774c93054641f6d7da20c56eca950ce94d0cfc8
ProgrammingHobby <Uwe.Merker@KabelBW.de>
2022-11-18 18:56:05 +0100

    Grundgerüst für die Klasse 'ImagesHistory' angelegt.

---
+++ CIFE/Sources/ImagesHistory.cpp
@@ -0,0 +1,30 @@
+
+/*
+ * imageshistory.cpp is part of CIFE
+ * Copyright (c) 2022  Uwe Merker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ImagesHistory.hpp"
+// --------------------------------------------------------------------------------
+ImagesHistory::ImagesHistory() {
+    // TODO Auto-generated constructor stub
+}
+
+// --------------------------------------------------------------------------------
+ImagesHistory::~ImagesHistory() {
+    // TODO Auto-generated destructor stub
+}
+
+// --------------------------------------------------------------------------------
---
+++ CIFE/Sources/ImagesHistory.hpp
@@ -0,0 +1,42 @@
+/*
+ * imageshistory.hpp is part of CIFE
+ * Copyright (c) 2022  Uwe Merker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef IMAGESHISTORY_HPP_
+#define IMAGESHISTORY_HPP_
+// --------------------------------------------------------------------------------
+#include <wx/object.h>
+// --------------------------------------------------------------------------------
+class ImagesHistory: public wxObject {
+    public: // Attributes
+
+    public: // Methods
+
+    public: // Constructor & Destructor
+        ImagesHistory();
+        virtual ~ImagesHistory();
+
+    protected: // Attributes
+
+    protected: // Methods
+
+    private: // Attributes
+
+    private: // Methods
+};
+
+// --------------------------------------------------------------------------------
+#endif /* IMAGESHISTORY_HPP_ */
